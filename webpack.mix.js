13c6a65fc071fda45322b682347d3e7aae5697ca
Oliver Davies <oliver@oliverdavies.uk>
2019-06-23 22:27:00 +0100

    Add PurgeCSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,11 @@
 const mix = require('laravel-mix')
 
+require('laravel-mix-purgecss')
+
 mix.postCss('resources/css/app.css', 'public/build/css', [
   require('tailwindcss')()
 ])
+
+mix.purgeCss({
+  folders: ['public/views']
+})
